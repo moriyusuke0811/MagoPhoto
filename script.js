98e94af36c98bf1d35c10513186cbaf9fcf56418
moriyusuke0811 <Moriyusuke0811@gmail.com>
2024-11-17 14:01:47 +0900

    d

--- script.js
+++ script.js
@@ -89,9 +89,7 @@ function uploadFile() {
 
 // Google APIライブラリの読み込みと初期化
 function handleClientLoad() {
-    gapi.load('client:auth2', () => {
-        initApiClient(); // Google APIの初期化が完了したら、次に進む
-    });
+    gapi.load('client:auth2', initApiClient); // Google APIの初期化が完了したら、次に進む
 }
 
 // 画像プレビューを表示する
